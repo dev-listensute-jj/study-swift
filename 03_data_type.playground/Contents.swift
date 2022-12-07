48c8bdfb1f422884babcd490b62564e05dfe9740
이재진 <dev-listensute-jj@gmail.com>
2022-12-07 16:08:45 +0900

    Changed Name of files

---
+++ 03_data_type.playground/Contents.swift
@@ -0,0 +1,30 @@
+// 기본 데이터 타입
+
+import UIKit
+
+// Swift 의 기본 데이터 타입
+// Bool, Int, UInt, Float, Double, character, String
+
+// 1. Bool
+var someBool: Bool = true
+someBool = false
+//someBool = 0 // Error !
+
+// 2. Int
+var someInt: Int = -100
+//someInt = 100.1 // Error !
+
+// 3. UInt
+var someUInt: UInt = 100
+//someUInt = -100 // Error !
+//someUInt = SomeInt // Error !
+
+// 4. Float
+var someFloat: Float = 3.14
+someFloat = 3
+
+// 5. Double
+var someDouble: Double = 3.14
+someDouble = 3
+//someDouble = someFloat // Error !
+
